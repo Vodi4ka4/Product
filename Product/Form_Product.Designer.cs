67918250d1ec1adc3675300aea69c3558df5ea5a
Vodi4ka4 <kolya.hrenov2018@gmail.com>
2024-03-14 20:40:55 +0300

    Добавьте файлы проекта.

---
+++ Product/Form_Product.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Product
+{
+    partial class Form_Product
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_search = new System.Windows.Forms.TextBox();
+            this.comboBox_Sorting = new System.Windows.Forms.ComboBox();
+            this.comboBox_filtration = new System.Windows.Forms.ComboBox();
+            this.flowLayoutPanel_Product = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel_Pagination = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox_search
+            // 
+            this.textBox_search.Location = new System.Drawing.Point(22, 26);
+            this.textBox_search.Name = "textBox_search";
+            this.textBox_search.Size = new System.Drawing.Size(380, 20);
+            this.textBox_search.TabIndex = 0;
+            this.textBox_search.TextChanged += new System.EventHandler(this.textBox_search_TextChanged);
+            // 
+            // comboBox_Sorting
+            // 
+            this.comboBox_Sorting.FormattingEnabled = true;
+            this.comboBox_Sorting.Items.AddRange(new object[] {
+            "Без сортировки",
+            "По возрастанию цены",
+            "По убыванию цены",
+            "По названию от а до я",
+            "По названию от я до а"});
+            this.comboBox_Sorting.Location = new System.Drawing.Point(408, 25);
+            this.comboBox_Sorting.Name = "comboBox_Sorting";
+            this.comboBox_Sorting.Size = new System.Drawing.Size(175, 21);
+            this.comboBox_Sorting.TabIndex = 1;
+            this.comboBox_Sorting.Text = "Сортировка";
+            this.comboBox_Sorting.SelectedIndexChanged += new System.EventHandler(this.comboBox_Sorting_SelectedIndexChanged);
+            // 
+            // comboBox_filtration
+            // 
+            this.comboBox_filtration.FormattingEnabled = true;
+            this.comboBox_filtration.Items.AddRange(new object[] {
+            "Все типы",
+            "Кружки",
+            "Тарелки",
+            "Столовые приборы"});
+            this.comboBox_filtration.Location = new System.Drawing.Point(589, 25);
+            this.comboBox_filtration.Name = "comboBox_filtration";
+            this.comboBox_filtration.Size = new System.Drawing.Size(157, 21);
+            this.comboBox_filtration.TabIndex = 2;
+            this.comboBox_filtration.Text = "Фильтрация";
+            this.comboBox_filtration.SelectedIndexChanged += new System.EventHandler(this.comboBox_filtration_SelectedIndexChanged);
+            // 
+            // flowLayoutPanel_Product
+            // 
+            this.flowLayoutPanel_Product.AutoScroll = true;
+            this.flowLayoutPanel_Product.Location = new System.Drawing.Point(22, 52);
+            this.flowLayoutPanel_Product.Name = "flowLayoutPanel_Product";
+            this.flowLayoutPanel_Product.Size = new System.Drawing.Size(766, 394);
+            this.flowLayoutPanel_Product.TabIndex = 5;
+            // 
+            // flowLayoutPanel_Pagination
+            // 
+            this.flowLayoutPanel_Pagination.Location = new System.Drawing.Point(630, 452);
+            this.flowLayoutPanel_Pagination.Name = "flowLayoutPanel_Pagination";
+            this.flowLayoutPanel_Pagination.Size = new System.Drawing.Size(158, 31);
+            this.flowLayoutPanel_Pagination.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Введите для поиска:";
+            // 
+            // Form_Product
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 495);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel_Pagination);
+            this.Controls.Add(this.flowLayoutPanel_Product);
+            this.Controls.Add(this.comboBox_filtration);
+            this.Controls.Add(this.comboBox_Sorting);
+            this.Controls.Add(this.textBox_search);
+            this.Name = "Form_Product";
+            this.Text = "Товары";
+            this.Load += new System.EventHandler(this.Form_Product_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_search;
+        private System.Windows.Forms.ComboBox comboBox_Sorting;
+        private System.Windows.Forms.ComboBox comboBox_filtration;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel_Product;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel_Pagination;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Product/ListItem.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Product
+{
+    partial class ListItem
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox_product = new System.Windows.Forms.PictureBox();
+            this.label_article = new System.Windows.Forms.Label();
+            this.label_name_product = new System.Windows.Forms.Label();
+            this.label_coust = new System.Windows.Forms.Label();
+            this.label_coust_number = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_product)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox_product
+            // 
+            this.pictureBox_product.Image = global::Product.Properties.Resources.no_image_copy;
+            this.pictureBox_product.Location = new System.Drawing.Point(15, 12);
+            this.pictureBox_product.Name = "pictureBox_product";
+            this.pictureBox_product.Size = new System.Drawing.Size(100, 83);
+            this.pictureBox_product.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox_product.TabIndex = 0;
+            this.pictureBox_product.TabStop = false;
+            // 
+            // label_article
+            // 
+            this.label_article.AutoSize = true;
+            this.label_article.Location = new System.Drawing.Point(169, 41);
+            this.label_article.Name = "label_article";
+            this.label_article.Size = new System.Drawing.Size(48, 13);
+            this.label_article.TabIndex = 1;
+            this.label_article.Text = "Артикул";
+            // 
+            // label_name_product
+            // 
+            this.label_name_product.AutoSize = true;
+            this.label_name_product.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_name_product.Location = new System.Drawing.Point(320, 33);
+            this.label_name_product.Name = "label_name_product";
+            this.label_name_product.Size = new System.Drawing.Size(232, 24);
+            this.label_name_product.TabIndex = 2;
+            this.label_name_product.Text = "Наименование продукта";
+            // 
+            // label_coust
+            // 
+            this.label_coust.AutoSize = true;
+            this.label_coust.Location = new System.Drawing.Point(646, 33);
+            this.label_coust.Name = "label_coust";
+            this.label_coust.Size = new System.Drawing.Size(62, 13);
+            this.label_coust.TabIndex = 3;
+            this.label_coust.Text = "Стоимость";
+            // 
+            // label_coust_number
+            // 
+            this.label_coust_number.AutoSize = true;
+            this.label_coust_number.Location = new System.Drawing.Point(667, 55);
+            this.label_coust_number.Name = "label_coust_number";
+            this.label_coust_number.Size = new System.Drawing.Size(13, 13);
+            this.label_coust_number.TabIndex = 4;
+            this.label_coust_number.Text = "0";
+            // 
+            // ListItem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.Controls.Add(this.label_coust_number);
+            this.Controls.Add(this.label_coust);
+            this.Controls.Add(this.label_name_product);
+            this.Controls.Add(this.label_article);
+            this.Controls.Add(this.pictureBox_product);
+            this.Name = "ListItem";
+            this.Size = new System.Drawing.Size(725, 108);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_product)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox_product;
+        private System.Windows.Forms.Label label_article;
+        private System.Windows.Forms.Label label_name_product;
+        private System.Windows.Forms.Label label_coust;
+        private System.Windows.Forms.Label label_coust_number;
+    }
+}
